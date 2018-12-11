1e8f320ea0aa39cb82349b5e93a2971e85b5a8e5
dangtruong <mactruong>
2018-12-11 16:26:02 +0700

    Chapter 10

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -25,7 +25,7 @@ module SessionsHelper
     current_user.present?
   end
 
-  def forget(user)
+  def forget user
     user.forget
     cookies.delete(:user_id)
     cookies.delete(:remember_token)
@@ -35,5 +35,18 @@ module SessionsHelper
     forget(current_user)
     session.delete :user_id
     @current_user = nil
+  end
+
+   def redirect_back_or default
+    redirect_to session[:forwarding_url] || default
+    session.delete :forwarding_url
+  end
+
+   def store_location
+    session[:forwarding_url] = request.original_url if request.get?
+  end
+
+  def current_user? user
+    user == current_user
   end
 end
